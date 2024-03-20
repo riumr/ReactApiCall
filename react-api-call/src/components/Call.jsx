7c0cd2d414ab9d7ac18998c62a7c7d7b1cf604bf
Kimtaehyung <108637691+riumr@users.noreply.github.com>
2024-03-20 21:49:29 +0900

    feat : fetch->axios api 호출 방법 변경

--- react-api-call/src/components/Call.jsx
+++ react-api-call/src/components/Call.jsx
@@ -1,22 +1,25 @@
 import { useState, useEffect } from 'react';
+import axios from "axios";
 
 export default function Call() {
   const [data, setData] = useState(null);
-
+  const apiKey = process.env.REACT_APP_SEOUL_API_KEY;
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
     try {
-      const response = await fetch('')
-      console.log(response)
+      const response = await axios.get(`http://openapi.seoul.go.kr:8088/${apiKey}/json/citydata_ppltn/1/5/광화문·덕수궁`);
+      setData(response.data)
+      console.log(response.data)
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
   return (
     <>
+  
     </>
   )
 }
