555cceb2b4ad1d3b71fb912e2ec5d6e4e81cb91d
Kimtaehyung <108637691+riumr@users.noreply.github.com>
2024-03-22 23:37:45 +0900

    feat : 호출값 파싱

--- react-api-call/src/components/Call.jsx
+++ react-api-call/src/components/Call.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from "axios";
 
 export default function Call() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState(String);
   const apiKey = process.env.REACT_APP_API_KEY;
   useEffect(() => {
     fetchData();
@@ -11,15 +11,16 @@ export default function Call() {
   const fetchData = async () => {
     try {
       const response = await axios.get(`http://openapi.seoul.go.kr:8088/${apiKey}/json/citydata_ppltn/1/5/광화문·덕수궁`);
-      setData(response.data)
-      console.log(response.data)
+      setData(response.data['SeoulRtd.citydata_ppltn'][0]['AREA_PPLTN_MAX'])
+      console.log(response.data['SeoulRtd.citydata_ppltn'][0]['AREA_PPLTN_MAX'])
+      console.log(data)
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
   return (
     <>
-  
+      {data}
     </>
   )
 }
