5b04b87b34d04fe46803156d7a576acc423b11d4
Kimtaehyung <108637691+riumr@users.noreply.github.com>
2024-03-26 22:57:20 +0900

    feat : 가져오는 parameter 값 변경
    - 최대, 최솟값 -> 텍스트 지표

--- react-api-call/src/components/Call.jsx
+++ react-api-call/src/components/Call.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from "axios";
 
 export default function Call() {
-  const [data, setData] = useState(0);
+  const [data, setData] = useState("");
   const apiKey = process.env.REACT_APP_API_KEY;
   useEffect(() => {
     fetchData();
@@ -15,25 +15,17 @@ export default function Call() {
         axios.get(`http://openapi.seoul.go.kr:8088/${apiKey}/json/citydata_ppltn/1/5/서울대공원`)
       ])
       setData({
-        data01Max:parseInt(response01.data['SeoulRtd.citydata_ppltn'][0]['AREA_PPLTN_MAX']),
-        data01Min:parseInt(response01.data['SeoulRtd.citydata_ppltn'][0]['AREA_PPLTN_MIN']),
-        data02:parseInt(response02.data['SeoulRtd.citydata_ppltn'][0]['AREA_PPLTN_MAX'])
+        data01:response01.data['SeoulRtd.citydata_ppltn'][0]['AREA_CONGEST_LVL'],
+        data02:response02.data['SeoulRtd.citydata_ppltn'][0]['AREA_CONGEST_LVL']
       });
       console.log(data.data01Max-data.data01Min)
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
-  const data01Style={
-    height:parseInt(`${data.data01Max}-${data.data01Min}`)/100,
-    backgroundColor:'gray'
-  }
   return (
     <>
-      <div style={data01Style}>
-        {data.data01Max}
-        {data.data01Min}
-      </div>
+      <div>{data.data01}</div>
       <div>{data.data02}</div>
     </>
   )
