3f11f76541517905fdd9cbca31e9b68c15ca3821
Kimtaehyung <108637691+riumr@users.noreply.github.com>
2024-03-30 22:21:59 +0900

    feat : 장소list의 장소를 매개변수로 api 호출
    - 초기 렌더링 시에만 호출
    - 주기적 api 호출 필요

--- react-api-call/src/components/Call.jsx
+++ react-api-call/src/components/Call.jsx
@@ -1,29 +1,27 @@
-import { useState, useEffect } from 'react';
 import axios from "axios";
 import { placeList } from './PlaceList';
 
 export default function Call() {
-  const [data, setData] = useState("");
+  const places=placeList;
   const apiKey = process.env.REACT_APP_API_KEY;
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   const fetchData = async () => {
     try {
-      for (const place of placeList) {
-        const response = axios.get(`http://openapi.seoul.go.kr:8088/${apiKey}/json/citydata_ppltn/1/5/${place}`);
-        setData({
-          data01:response.data['SeoulRtd.citydata_ppltn'][0]['AREA_CONGEST_LVL'],
-        });
-      }
+      places.forEach(place=>{
+        axios.get(`http://openapi.seoul.go.kr:8088/${apiKey}/json/citydata_ppltn/1/5/${place}`)
+        .then(response=>{
+          // 데이터가 없는 경우 제외하고 모두 호출
+          if(response.data['RESULT.CODE']!=='INFO-200'){
+            console.log(response.data['SeoulRtd.citydata_ppltn'][0]['AREA_CONGEST_LVL']);
+          }
+        })
+      })
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
+  fetchData();
   return (
     <>
-      <div>{data.data01}</div>
     </>
   )
 }
